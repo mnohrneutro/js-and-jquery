7b22762b79042539c89e64db41e51bef260a7916
mnohrneutro <manohar.neutroline@gmail.com>
2021-06-10 20:00:49 +0545

    creating a basic structure of javascript

---
+++ c01/js/script.js
@@ -0,0 +1,15 @@
+let today = new Date();
+let hourNow = today.getHours();
+let greeting;
+
+if (hourNow > 18) {
+    greeting = 'good evening';
+} else if (hourNow > 12) {
+    greeting = 'good afternoon';
+} else if (hourNow > 0) {
+    greeting = 'good morning';
+} else {
+    greeting = 'welcome';
+}
+
+document.write('<h3>'+greeting+'</h3>');
